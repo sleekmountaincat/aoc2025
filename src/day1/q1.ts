bf957cc1c8bb4cef22703dbc1b140a2abcce8a29
chris nelson <chris.nelson@harperdb.io>
2025-12-01 19:26:54 -0700

    q1

--- src/day1/q1.ts
+++ src/day1/q1.ts
@@ -1,7 +1,26 @@
 import fs from "fs";
 
+let dial = 50;
+let cnt = 0;
+
 const map = fs
   .readFileSync("src/day1/input.txt")
   .toString()
   .split("\n")
-  .forEach(l=>console.log(l));
+  .forEach(l=>{
+    const d = l[0];
+    const n = +l.slice(1) % 100;
+
+    let newDial = d === "L" ? dial - n : dial + n;
+
+    if (newDial < 0) {
+      newDial += 100;
+    } else if (newDial > 99) {
+      newDial -= 100;
+    }
+
+    if (newDial === 0) cnt++;
+    console.log(dial, d, n, newDial, cnt)
+
+    dial = newDial;
+  });
