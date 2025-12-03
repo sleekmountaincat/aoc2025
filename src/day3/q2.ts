233a883b018867bc82e76d5d00fab751f1ac1520
chris nelson <chris.nelson@harperdb.io>
2025-12-03 01:16:42 -0700

    q2

---
+++ src/day3/q2.ts
@@ -0,0 +1,23 @@
+import fs from "fs";
+
+let cnt = 0;
+const s = 12;
+
+const map = fs
+  .readFileSync("src/day3/input.txt")
+  .toString()
+  .split("\n")
+  .forEach(l=>{
+    let bank = l.split("").map(Number);
+    let n = '';
+
+    for(let i = s-1; i >= 1; i--) {
+      const nextDigit = Math.max(...bank.slice(0,-i));
+      n += nextDigit.toString()
+      bank = bank.slice(bank.indexOf(nextDigit)+1);
+    }
+
+    cnt += +(n+Math.max(...bank).toString());
+  });
+
+  console.log(cnt)
